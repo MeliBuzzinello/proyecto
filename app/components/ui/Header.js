fb94489055ba3f59d56c3bfa94d0ff7f02a01a76
melisa <melisabuzzinello@gmail.com>
2023-11-07 19:28:01 -0300

    test

--- app/components/ui/Header.js
+++ app/components/ui/Header.js
@@ -8,6 +8,9 @@ const Header = () => {
         <div className="container m-auto py-6 flex justify-between items-center">
           <Image alt="Logo" src={logo} width={200} height={50} />
         </div>
+        <div className="container m-auto py-6 flex justify-between items-center">
+          <Image alt="Logo" src={logo} width={200} height={50} />
+        </div>
       </header>
     </>
   );
